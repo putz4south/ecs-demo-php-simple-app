f4b5fa1d2ff906af4130845ee62288deb83bb0d2
Phuthuma Mvovo <pmvovo@deloitte.co.za>
2018-02-01 09:55:00 +0200

    deleted comment

--- src/index.php
+++ src/index.php
@@ -18,7 +18,6 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-		<-- <p>We made a change. Has it been deployed?</p> !-->
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
